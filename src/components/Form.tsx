e83d45f4dd8c62f02e3864efb451b48bd9ccc27f
Mewan Dimalsha <115539818+mewanDimalsha@users.noreply.github.com>
2024-03-25 13:14:18 +0530

    testing project

---
+++ src/components/Form.tsx
@@ -0,0 +1,39 @@
+import { FieldValues, useForm } from "react-hook-form";
+
+const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="md-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="md-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
